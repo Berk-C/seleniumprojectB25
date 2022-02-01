909f1ab6bd6508fcc65d343f3cc799d73b1dfbcf
canka <cankaya439@gmail.com>
2022-02-01 14:44:46 -0600

    initial commit

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -5,50 +5,66 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigations {
+
     public static void main(String[] args) throws InterruptedException {
 
-        //1- setup the browser driver
+        //1- Set up the "browser driver"
         WebDriverManager.chromedriver().setup();
 
-        //2- Create instance of the chrome driver
+        //2- Create instance of the Selenium WebDriver
+        // This is the line opening an empty browser
         WebDriver driver = new ChromeDriver();
 
-        //3- Go to tesla.com
-        driver.get("https://www.bmwusa.com");
-        String currentTitle = driver.getTitle();
+        //3- Go to "https://www.tesla.com"
+        driver.get("https://www.bmwUSA.com");
 
+        //get the title of the page
+        String currentTitle = driver.getTitle();
         System.out.println("currentTitle = " + currentTitle);
 
-        //Stop code execution for 3 second
+        String currentURL =  driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentURL);
+
+        //Stop code execution for 3 seconds
         Thread.sleep(3000);
 
-        //Use selenium to navigate back
+        // use selenium to navigate back
         driver.navigate().back();
 
-        //Stop code execution for 3 second
+        //Stop code execution for 3 seconds
         Thread.sleep(3000);
 
         // use selenium to navigate forward
         driver.navigate().forward();
 
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
         // use selenium to navigate refresh
         driver.navigate().refresh();
 
-        //Stop code execution for 3 second
+        //Stop code execution for 3 seconds
         Thread.sleep(3000);
 
-        //use navigate().to():
-        driver.navigate().to("https://google.com");
-        currentTitle = driver.getTitle();
-        
-        // use .getTitle();
-        //System.out.println("driver.getTitle() = " + driver.getTitle());
+        // use navigate().to():
+        driver.navigate().to("https://www.google.com");
 
+        //get the current title after getting the google page
+        currentTitle = driver.getTitle();
+
+
+        //System.out.println("driver.getTitle() = " + driver.getTitle());
 
         System.out.println("currentTitle = " + currentTitle);
 
-        // use getCurrent URL
-        String currentUrl = driver.getCurrentUrl();
+        // Get the current URL using Selenium
+        currentURL = driver.getCurrentUrl();
+
+        System.out.println("currentURL = " + currentURL);
+
+
 
     }
+
+
 }
