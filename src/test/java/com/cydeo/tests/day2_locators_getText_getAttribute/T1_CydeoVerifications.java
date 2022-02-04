56edf0d81ba10a83a83d7314ede92fc9b3da7130
canka <cankaya439@gmail.com>
2022-02-04 09:46:27 -0600

    initial commit

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -1,4 +1,45 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_CydeoVerifications {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://practice.cydeo.com");
+
+
+        String expectedURL = "cydeo";
+
+        String actualURL = driver.getCurrentUrl();
+
+
+        if(actualURL.contains(expectedURL)){
+            System.out.println("URL PASS");
+        } else {
+            System.out.println("URL FAILED");
+        }
+
+
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title is matched");
+        }else {
+            System.out.println("Title is not matched");
+        }
+
+
+
+    }
 }
